4436893dd44ceede097199b2e5fec9211d8a8cd2
shruti singh <shrutisinghskr@gmail.com>
2026-02-08 20:08:27 +0530

    refactor: update websocket server connection handling and message formatting

--- server.js
+++ server.js
@@ -1,19 +1,49 @@
+import { error } from "node:console";
 import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8000 });
 
 console.log("Websocket is running on ws://localhost:8000");
 
-wss.on('connection', (socket, request) => {
-    const ip = request.socket.remoteAddress;
-    console.log(`New Client got connected from ${ip} address`);
+wss.on("connection", (socket, request) => {
+  const clientIp = request.socket.remoteAddress;
+  console.log(`New Client got connected from ${ip} address`);
 
-    // server
-    socket.send(JSON.stringify({
-        type: 'welcome',
-        message: 'Welcome to websocket server!'
-    }));
+  // server
+  socket.send(
+    JSON.stringify({
+      type: "welcome",
+      message: "Welcome to websocket server!",
+    }),
+  );
 
-    // client
-    
-})
+  // client
+  socket.on("message", (rawData) => {
+    console.log(`📨 Received:`, rawData.toString());
+
+    const message = rawData.toString();
+
+    // broardcasting to the all connected clients
+    wss.clients.forEach((client) => {
+      // only send if the client connection is open
+      if (client.readyState === 1) {
+        client.send(
+          JSON.stringify({
+            type: "broadcast",
+            message: message,
+          }),
+        );
+      }
+    });
+  });
+});
+
+// handle err or we'll be cooked
+socket.on("errors", (error) => {
+  console.error(`❌ Error for ${clientIp} : `, error.message);
+});
+
+// handle disconnection
+socket.on("close", () => {
+  console.log(`👋Client got diconnected: ${clientIp}`);
+});
